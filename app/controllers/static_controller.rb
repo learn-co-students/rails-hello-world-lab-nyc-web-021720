2cdcae550c869befb0042a3ca3963727ee1f9a3b
codenameuriel <u.e.rodriguez@gmail.com>
2020-03-11 12:29:38 -0400

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+class StaticController < ApplicationController
+
+  def hello_world
+    #implicit rendering
+    # finds file within static directory with
+    # the same name as this method 
+  end
+
+end
